dbd625b815a62c9e3810449d8e5941fe9bd16027
erkinSultanakimov <erkin.sultanakimov@gmail.com>
2021-02-24 18:06:49 +0600

    Убрал лишнее условие "IF", добавил условие "else if", и "else" для отбрасывания случаи не входящих в условия. Использовал рандомное число как аргумент для  возраста. Существенно сократил код.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,38 +5,40 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(ageTemperatureOutdoor( 21,  -15 ));
+        System.out.println(ageTemperatureOutdoor(21, -15));
         System.out.println(ageTemperatureOutdoor(23, 4));
-        System.out.println(ageTemperatureOutdoor(25,35));
-        System.out.println(ageTemperatureOutdoor(6,20));
-        System.out.println(ageTemperatureOutdoor(46,26));
-        System.out.println(ageTemperatureOutdoor(27,10));
-        System.out.println(ageTemperatureOutdoor(generateRandomAge(),25));
+        System.out.println(ageTemperatureOutdoor(25, 35));
+        System.out.println(ageTemperatureOutdoor(6, 20));
+        System.out.println(ageTemperatureOutdoor(46, 26));
+        System.out.println(ageTemperatureOutdoor(27, 10));
+
+        System.out.println(ageTemperatureOutdoor(generateRandomAge(), 25));
+        //System.out.println("Рандомный возраст");
     }
-   public static String ageTemperatureOutdoor(int age, int temperature){
-       System.out.println("Возраст " + age + "; " + "Температура на улице " +temperature);
 
-       if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)){
-           System.out.println("Можно идти гулять");}
-           else if ((age < 20) && (temperature > 0 && temperature < 28))
-           {
-               System.out.println("Можно идти гулять");
-           }
-        else if (age > 45 && temperature > -10 && temperature < 25)
-        {
-            System.out.println("Можно идти гулять");
+    public static String ageTemperatureOutdoor(int age, int temperature) {
+
+        if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)) {
+            return"Можно идти гулять";
+        } else if ((age < 20) && (temperature > 0 && temperature < 28)) {
+            return "Можно идти гулять";
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
         }
-          else    { System.out.println("Оставайтесь дома"); }
-         return " ";
 
-   }
-   public static int generateRandomAge()
-   { int age = (int)(Math.random()*50);
-       System.out.println("Рандомный возраст " + age);
+        //return "Возраст " + age + "; " + "Температура на улице " + temperature;
 
-       return age;
+    }
+
+    public static int generateRandomAge() {
+        int age = (int) (Math.random() * 50);
 
 
-   }
+        return age;
+
+
+    }
 
 }
